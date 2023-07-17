ef45aaa81c87920107c5d62980fa9d9623d19125
yangchenguang <yangchenguang@fpwis.com>
2023-07-17 16:01:56 +0800

    feat: home and layout 样式修改

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: yangchenguang
+ * @Description: tailwindcss 配置
+ * @Date: 2023-07-07 15:51:45
+ * @LastEditors: yangchenguang
+ * @LastEditTime: 2023-07-17 15:01:06
+ */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
